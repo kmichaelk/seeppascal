d3e828951d7d585d3ee97173a8aa048b6a9beb02
kmichaelk <130953568+kmichaelk@users.noreply.github.com>
2024-05-13 07:16:29 +0300

    Add missing program name getter

--- interpreter/seepctx.cpp
+++ interpreter/seepctx.cpp
@@ -9,8 +9,12 @@ Seep::Context::Context(State* state)
     : state(state)
 {}
 
-Seep::Runtime&  Seep::Context::get_runtime() const {
+Seep::Runtime& Seep::Context::get_runtime() const {
     return *state->runtime;
+}
+
+const std::string& Seep::Context::get_program_name() const {
+    return state->program_name;
 }
 
 const std::unordered_map<std::string, Seep::MemRec>& Seep::Context::vars() const {
